6a3eb2da7444612b4f03d4a1c283d385443f74d4
NikitoriusMinsk <45353693+NikitoriusMinsk@users.noreply.github.com>
2023-01-30 11:14:20 +0400

    adjust mobile layout

--- src/components/layout/MobileLayout.tsx
+++ src/components/layout/MobileLayout.tsx
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import styles from "@styles/components/layout/MobileLayout.module.css";
 import Footer from "./Footer";
 import MobileHeader from "./MobileHeader";
 import MobileNavbar from "./MobileNavbar";
+import MobileTipsterHeader from "./MobileTipsterHeader";
+import { useRouter } from "next/router";
 
 interface MobileLayoutProps {
 	children?: JSX.Element | JSX.Element[];
@@ -10,9 +12,21 @@ interface MobileLayoutProps {
 
 const MobileLayout: React.FC<MobileLayoutProps> = (props) => {
 	const { children } = props;
+	const router = useRouter();
+
+	const tipsterRoutes = ["/tipster-rating", "/tipster-competition"];
+
+	function getHeader(): ReactElement {
+		if (tipsterRoutes.includes(router.asPath.split("?")[0] ?? "")) {
+			return <MobileTipsterHeader />;
+		}
+
+		return <MobileHeader />;
+	}
+
 	return (
 		<>
-			<MobileHeader />
+			{getHeader()}
 			<div className={styles.pageWrap}>
 				<div className={styles.content}>{children}</div>
 				<Footer />
---
+++ src/components/layout/MobileTipsterHeader.tsx
@@ -0,0 +1,48 @@
+import React from "react";
+import styles from "@styles/components/layout/MobileHeader.module.css";
+import Link from "next/link";
+import Image from "next/future/image";
+import UserProfile from "./shared/UserProfile";
+import { trpc } from "src/utils/trpc";
+import MenuLink from "./shared/MenuLink";
+import { useRouter } from "next/router";
+
+const links = [
+	{ href: "/tipster-rating", label: "Tipsters" },
+	{ href: "/tipster-competition", label: "Competition" },
+];
+
+const MobileTipsterHeader: React.FC = () => {
+	const router = useRouter();
+
+	return (
+		<div className={styles.container}>
+			<div className={styles.row}>
+				<Link href={"/"}>
+					<a className={styles.logo}>
+						<Image
+							src="/logo.svg"
+							height={32}
+							width={188}
+							alt=""
+						/>
+					</a>
+				</Link>
+				<UserProfile />
+			</div>
+			{links && (
+				<nav className={styles.tipsterNavigation}>
+					{links.map((link) => (
+						<MenuLink
+							key={link.label}
+							{...link}
+							active={router.pathname.includes(link.href)}
+						/>
+					))}
+				</nav>
+			)}
+		</div>
+	);
+};
+
+export default MobileTipsterHeader;
