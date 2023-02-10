c61619afaf216331a7b0f5edc11a55417033854d
MaX2708 <grin.maxim.2708@yandex.ru>
2023-02-10 16:39:56 +0400

    page slider ultimate fix

--- src/components/ui/match-summary/PagesSlider.tsx
+++ src/components/ui/match-summary/PagesSlider.tsx
@@ -1,47 +1,17 @@
-import React, { createRef, MouseEvent } from "react"
+import React, { useRef, MouseEvent } from "react"
 import styles from "../../../styles/pages/MatchSummary.module.css"
+import { useDraggable } from "react-use-draggable-scroll";
 
 interface PagesSliderProps {
     children: React.ReactNode
 }
 
-interface posProps {
-    left: number
-    x: number
-}
-
 const PagesSlider:React.FC<PagesSliderProps> = ({children}) => {
-    const sliderRef = createRef<HTMLDivElement>();
-    let pos:posProps = { left: 0, x: 0 };
-
-    const mouseDownHandler = function (e: MouseEvent<HTMLDivElement>) {
-        pos = {
-            // The current scroll
-            left: sliderRef.current?.scrollLeft || 0,
-            // Get the current mouse position
-            x: e.clientX,
-        };
-        // sliderRef.current?.style.userSelect = 'none';
-        document.addEventListener('mousemove', mouseMoveHandler);
-        document.addEventListener('mouseup', mouseUpHandler);
-        // e.currentTarget.style.pointerEvents = 'none'
-    };
-
-    const mouseMoveHandler = function (e: any) {
-        // How far the mouse has been moved
-        const dx = e.clientX - pos.x;
-        // Scroll the element
-        if (sliderRef && sliderRef.current)
-        sliderRef.current!.scrollLeft = pos.left - dx;
-    };
-
-    const mouseUpHandler = function () {
-        document.removeEventListener('mousemove', mouseMoveHandler);
-        document.removeEventListener('mouseup', mouseUpHandler); 
-    }
+    const sliderRef = useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+    const { events } = useDraggable(sliderRef);
 
     return (
-        <div className={styles.pageSelector} onMouseDown={mouseDownHandler} ref={sliderRef}>
+        <div className={styles.pageSelector} {...events} ref={sliderRef}>
             <div className={styles.pagesLine}>
                 {children}
             </div>            
