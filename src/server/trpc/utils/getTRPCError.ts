10823048d01dcb3e03350172eb1fac2390845086
Nikita <nikitoriusminsk@gmail.com>
2023-07-27 12:32:26 +0400

    move trpc error codes into server types

--- src/server/trpc/utils/getTRPCError.ts
+++ src/server/trpc/utils/getTRPCError.ts
@@ -1,12 +1,12 @@
-import { TRPC_ERROR_CODES_BY_API_CODE } from "./trpcErrorCode"
+import { TRPC_ERROR_CODES_BY_API_CODE } from "./types/TRPCErrorCode";
 import TRPCExtendedError from "./types/TRPCExtendedError";
 
 export default async function getTRPCError(result: Response) {
-    const resObj = await result.json()
-    return new TRPCExtendedError({
-        code: TRPC_ERROR_CODES_BY_API_CODE[resObj.errorCode] ?? 'INTERNAL_SERVER_ERROR',
-        message: resObj.message,
-        cause: resObj,
-        serverError: resObj
-    })
+	const resObj = await result.json();
+	return new TRPCExtendedError({
+		code: TRPC_ERROR_CODES_BY_API_CODE[resObj.errorCode] ?? "INTERNAL_SERVER_ERROR",
+		message: resObj.message,
+		cause: resObj,
+		serverError: resObj,
+	});
 }
