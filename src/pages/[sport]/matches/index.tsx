0d40eb9df5b0dd0fe5e97aaed3df8ff1dd2f764f
MaX2708 <grin.maxim.2708@yandex.ru>
2023-08-31 14:07:07 +0400

    matches page <=1366 - bottom blocks design fix

--- src/pages/[sport]/matches/index.tsx
+++ src/pages/[sport]/matches/index.tsx
@@ -1,42 +1,52 @@
-import React, { useContext, useReducer, useState } from "react";
-import { GetServerSideProps, GetStaticPaths, GetStaticProps, NextPage } from "next";
-import styles from "@styles/pages/Matches.module.css";
-import { trpc } from "src/utils/trpc";
-import BestBookmakers from "@components/ui/BestBookmakers";
-import Banner from "@components/ui/Banner";
-import Filter from "@components/ui/Filter";
-import DatePicker from "@components/ui/DatePicker";
-import LiveMatches from "@components/ui/LiveMatches";
-import Leagues from "@components/ui/Leagues";
-import NestedFilter from "@components/ui/NestedFilter";
-import { createProxySSGHelpers } from "@trpc/react-query/ssg";
-import { appRouter } from "src/server/trpc/router/_app";
-import { createContext } from "src/server/trpc/context";
-import superjson from "superjson";
-import useWindowSize from "src/utils/useWindowSize";
-import dynamic from "next/dynamic";
-import usePortal from "src/utils/usePortal";
-import { PortalContext } from "src/utils/portalContext";
-import TextField from "@components/ui/TextField";
-import FilterModal from "@components/ui/FilterModal";
-import DisaperingContainer from "@components/helpers/DisaperingContainer";
-import LeaguesMobileBlocksFilter from "@components/ui/LeaguesMobileBlocksFilter";
-import { LastSportContext } from "src/pages/_app";
-import moment from "moment-timezone";
+import React, { useContext, useReducer, useState } from "react"
+import {
+	GetServerSideProps,
+	GetStaticPaths,
+	GetStaticProps,
+	NextPage,
+} from "next"
+import styles from "@styles/pages/Matches.module.css"
+import { trpc } from "src/utils/trpc"
+import BestBookmakers from "@components/ui/BestBookmakers"
+import Banner from "@components/ui/Banner"
+import Filter from "@components/ui/Filter"
+import DatePicker from "@components/ui/DatePicker"
+import LiveMatches from "@components/ui/LiveMatches"
+import Leagues from "@components/ui/Leagues"
+import NestedFilter from "@components/ui/NestedFilter"
+import { createProxySSGHelpers } from "@trpc/react-query/ssg"
+import { appRouter } from "src/server/trpc/router/_app"
+import { createContext } from "src/server/trpc/context"
+import superjson from "superjson"
+import useWindowSize from "src/utils/useWindowSize"
+import dynamic from "next/dynamic"
+import usePortal from "src/utils/usePortal"
+import { PortalContext } from "src/utils/portalContext"
+import TextField from "@components/ui/TextField"
+import FilterModal from "@components/ui/FilterModal"
+import DisaperingContainer from "@components/helpers/DisaperingContainer"
+import LeaguesMobileBlocksFilter from "@components/ui/LeaguesMobileBlocksFilter"
+import { LastSportContext } from "src/pages/_app"
+import moment from "moment-timezone"
 
-const OutPortal = dynamic(async () => (await import("react-reverse-portal")).OutPortal, {
-	ssr: false,
-});
+const OutPortal = dynamic(
+	async () => (await import("react-reverse-portal")).OutPortal,
+	{
+		ssr: false,
+	}
+)
 
 const MatchesPage: NextPage = () => {
-	const sport = useContext(LastSportContext);
-	const [selectedTopLeagues, setSelectedTopLeagues] = useState<number[]>([]);
-	const [selectedLeagues, setSelectedLeagues] = useState<number[]>([]);
-	const [selecteDate, setSelectedDate] = useState<Date>(new Date());
-	const { data: tips, isLoading: tipsLoading } = trpc.tips.getAll.useQuery();
+	const sport = useContext(LastSportContext)
+	const [selectedTopLeagues, setSelectedTopLeagues] = useState<number[]>([])
+	const [selectedLeagues, setSelectedLeagues] = useState<number[]>([])
+	const [selecteDate, setSelectedDate] = useState<Date>(new Date())
+	const { data: tips, isLoading: tipsLoading } = trpc.tips.getAll.useQuery()
 	const { data: matches, isLoading: matchesLoading } =
 		trpc.matches.getAllByLeague.useQuery({
-			leagueId: Array.from(new Set(selectedLeagues.concat(selectedTopLeagues))),
+			leagueId: Array.from(
+				new Set(selectedLeagues.concat(selectedTopLeagues))
+			),
 			page: 0,
 			size:
 				selectedLeagues.concat(selectedTopLeagues).length === 0
@@ -44,21 +54,25 @@ const MatchesPage: NextPage = () => {
 					: selectedLeagues.concat(selectedTopLeagues).length,
 			sportId: sport.id,
 			date: moment(selecteDate).format("YYYY-MM-DD"),
-		});
+		})
 	const { data: bookmakers, isLoading: bookmakersLoading } =
-		trpc.bookmakers.getTop.useQuery();
+		trpc.bookmakers.getTop.useQuery()
 	const { data: leaguesTop, isLoading: leaguesTopLoading } =
-		trpc.filters.getTopLeaguesByCountry.useQuery({ sportId: sport.id });
+		trpc.filters.getTopLeaguesByCountry.useQuery({ sportId: sport.id })
 	const { data: leagues, isLoading: leaguesLoading } =
-		trpc.filters.getLeaguesByCountry.useQuery({ sportId: sport.id });
-	const { data: filters, isLoading: filtersLoading } = trpc.filters.getLeagues.useQuery(
-		{ page: 0, size: 20, sportId: sport.id }
-	);
-	const { data: sports, isLoading: sportsLoading } = trpc.filters.getSports.useQuery();
+		trpc.filters.getLeaguesByCountry.useQuery({ sportId: sport.id })
+	const { data: filters, isLoading: filtersLoading } =
+		trpc.filters.getLeagues.useQuery({
+			page: 0,
+			size: 20,
+			sportId: sport.id,
+		})
+	const { data: sports, isLoading: sportsLoading } =
+		trpc.filters.getSports.useQuery()
 	const { data: liveMatches, isLoading: liveMatchesLoading } =
-		trpc.matches.getAllLive.useQuery();
-	const { width } = useWindowSize();
-	const portalNode = usePortal();
+		trpc.matches.getAllLive.useQuery()
+	const { width } = useWindowSize()
+	const portalNode = usePortal()
 
 	if (
 		tipsLoading ||
@@ -69,7 +83,7 @@ const MatchesPage: NextPage = () => {
 		liveMatchesLoading ||
 		filtersLoading
 	) {
-		return <div>Loading...</div>;
+		return <div>Loading...</div>
 	}
 
 	if (
@@ -81,7 +95,7 @@ const MatchesPage: NextPage = () => {
 		!sports ||
 		!filters
 	) {
-		return <div>Error...</div>;
+		return <div>Error...</div>
 	}
 
 	return (
@@ -90,7 +104,9 @@ const MatchesPage: NextPage = () => {
 				{portalNode && <OutPortal node={portalNode} />}
 				<div className={styles.filters}>
 					<div>
-						<DatePicker onChange={(date) => setSelectedDate(date)} />
+						<DatePicker
+							onChange={(date) => setSelectedDate(date)}
+						/>
 						<TextField
 							icon="/icons/search.svg"
 							placeholder="Search"
@@ -101,12 +117,16 @@ const MatchesPage: NextPage = () => {
 						items={leaguesTop}
 						h3="BY COUNTRY"
 						h2="Choose Matches"
-						onChange={(countries, leagues) => setSelectedTopLeagues(leagues)}
+						onChange={(countries, leagues) =>
+							setSelectedTopLeagues(leagues)
+						}
 					/>
 					<NestedFilter
 						items={leagues}
 						h3="OTHER COUNTRIES"
-						onChange={(countries, leagues) => setSelectedLeagues(leagues)}
+						onChange={(countries, leagues) =>
+							setSelectedLeagues(leagues)
+						}
 						withClearButton={false}
 						colapsible={true}
 					/>
@@ -189,51 +209,55 @@ const MatchesPage: NextPage = () => {
 					className={styles.sideColumn}
 					condition={false}
 				>
-					<LiveMatches matches={liveMatches.slice(0, 3)} />
+					<div className={styles.wide1366}>
+						<LiveMatches matches={liveMatches.slice(0, 3)} />
+					</div>
 					<Banner
 						height={463}
 						image="/images/banner-placeholder-2.png"
 					/>
-					<BestBookmakers bookmakers={bookmakers} />
+					<div className={styles.wideBook1366}>
+						<BestBookmakers bookmakers={bookmakers} />
+					</div>
 				</DisaperingContainer>
 			</PortalContext.Provider>
 		</>
-	);
-};
+	)
+}
 
 export const getStaticPaths: GetStaticPaths = async () => {
 	return {
 		fallback: "blocking",
 		paths: [],
-	};
-};
+	}
+}
 
 export const getStaticProps: GetStaticProps = async (context) => {
 	const ssg = createProxySSGHelpers({
 		router: appRouter,
 		ctx: await createContext(),
 		transformer: superjson,
-	});
+	})
 
-	await ssg.tips.getAll.prefetch();
+	await ssg.tips.getAll.prefetch()
 	await ssg.matches.getAllByLeague.prefetch({
 		leagueId: [],
 		page: 1,
 		size: 5,
 		sportId: 1,
 		date: moment(new Date()).format("YYYY-MM-DD"),
-	});
-	await ssg.bookmakers.getTop.prefetch();
-	await ssg.filters.getLeaguesByCountry.prefetch({ sportId: 1 });
-	await ssg.filters.getSports.prefetch();
-	await ssg.matches.getAllLive.prefetch();
+	})
+	await ssg.bookmakers.getTop.prefetch()
+	await ssg.filters.getLeaguesByCountry.prefetch({ sportId: 1 })
+	await ssg.filters.getSports.prefetch()
+	await ssg.matches.getAllLive.prefetch()
 
 	return {
 		props: {
 			trpcState: ssg.dehydrate(),
 		},
 		revalidate: 60,
-	};
-};
+	}
+}
 
-export default MatchesPage;
+export default MatchesPage
