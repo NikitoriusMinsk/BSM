bcf9fd7fd063620026b9430c7dc6eb608a3ce4e4
MaX2708 <grin.maxim.2708@yandex.ru>
2023-09-12 13:30:45 +0400

    apicall fix for empty 200 response

--- src/server/trpc/utils/makeApiCall.ts
+++ src/server/trpc/utils/makeApiCall.ts
@@ -1,21 +1,25 @@
-import { env } from "src/env/server.mjs";
-import { z } from "zod";
-import getTRPCError from "./getTRPCError";
+import { env } from "src/env/server.mjs"
+import { z } from "zod"
+import getTRPCError from "./getTRPCError"
 
 export default async function makeApiCall<O extends object>(
 	path: string,
 	schema: z.Schema<O>,
 	options?: RequestInit
 ) {
-	const result = await fetch(`${env.API_URL}/${path}`, options);
+	const result = await fetch(`${env.API_URL}/${path}`, options)
 
 	if (result.ok) {
-		const response = await result.json();
+		try {
+			const response = await result.json()
 
-		const parsedObject = schema.parse(response);
+			const parsedObject = schema.parse(response)
 
-		return parsedObject;
+			return parsedObject
+		} catch {
+			if (result.status == 200) return {}
+		}
 	} else {
-		throw await getTRPCError(result);
+		throw await getTRPCError(result)
 	}
 }
